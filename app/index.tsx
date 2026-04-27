6b366c99a122e9ce94b15d8d1e8026f5cd460d0a
Julio Alan Trejo Magaña <alanhalo23@gmail.com>
2026-04-27 15:30:53 -0600

    Calculadora

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import Cacluladora from './Screens/Calculadora';
+
+export default function Index() {
+  return (
+    <View >
+      <Cacluladora />
+    </View>
+  );
+}
